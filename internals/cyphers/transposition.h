9a6e8eb1a95a8c0367915729e9e68fc48674b14c
Gojus1 <gojus.kastenas123@gmail.com>
2025-10-04 21:41:03 +0300

    Add files via upload

---
+++ internals/cyphers/transposition.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "../../util/string.h"
+#include "../../util/number.h"
+#include "../../util/misc.h"
+
+const char* transpositionEntry(const char* alph, const char* encText, const char* frag);
