8a2cafc578a49f1a4b5f9bbe9a5be1bc2c3b4c0d
Gojus1 <gojus.kastenas123@gmail.com>
2025-10-04 18:50:16 +0300

    Add files via upload

---
+++ internals/cyphers/skytales.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <time.h>
+#include "../../util/string.h"
+#include "../../util/number.h"
+#include "../../util/misc.h"
+
+const char* skytalesEntry(const char* alph, const char* encText, const char* frag);
